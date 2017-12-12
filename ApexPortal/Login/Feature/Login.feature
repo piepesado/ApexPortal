79677b72e4f3ab9b8ae07ce42c865c11c5d0409d
Diego Gastón <dienan16@gmail.com>
2017-12-12 15:28:04 -0300

    Add project files.

---
+++ ApexPortal/Login/Feature/Login.feature
@@ -0,0 +1,12 @@
+﻿Feature: Login
+	As a Agency Agent user,
+	I would like to be able to login into APEX Portal
+
+@login
+Scenario: Login as a Agency Agent
+	Given that I navigate to the APEX Portal Url
+	And I enter jimmie.carr@travelleaders.com as the username
+	And I enter the password
+	And I enter 94326 as the CID
+	When I click on Login button
+	Then I should land on Apex hompage for Agency Agent role
---
+++ ApexPortal/Login/Steps/LoginSteps.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using NUnit.Framework;
+using ApexPortal.Login.Pages;
+
+namespace ApexPortal.Login.Steps
+{
+    [Binding]
+    public class LoginSteps
+    {
+        [Given(@"that I navigate to the APEX Portal Url")]
+        public void GivenThatINavigateToTheAPEXPortalUrl()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I enter jimmie\.carr@travelleaders\.com as the username")]
+        public void GivenIEnterJimmie_CarrTravelleaders_ComAsTheUsername()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I enter the password")]
+        public void GivenIEnterThePassword()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I enter (.*) as the CID")]
+        public void GivenIEnterAsTheCID(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I click on Login button")]
+        public void WhenIClickOnLoginButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should land on Apex hompage for Agency Agent role")]
+        public void ThenIShouldLandOnApexHompageForAgencyAgentRole()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
